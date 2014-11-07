507d4119a2116cffa182c7258980a009e03f9d8d
avijittamu <avijit.chakraborty@tamu.edu>
2014-11-07 11:45:43 -0600

    csv feature added
    added csv dump for the users index page.

--- ypa-iteration0/app/controllers/users_controller.rb
+++ ypa-iteration0/app/controllers/users_controller.rb
@@ -80,4 +80,12 @@ class UsersController < ApplicationController
       format.json { head :no_content }
     end
   end
+  def index
+    @users = User.order(:first_name)
+    respond_to do |format|
+      format.html
+      format.csv { send_data @users.to_csv }
+      format.xls # { send_data @products.to_csv(col_sep: "\t") }
+    end
+  end
 end
--- ypa-iteration0/app/models/user.rb
+++ ypa-iteration0/app/models/user.rb
@@ -19,4 +19,13 @@ class User < ActiveRecord::Base
   # associate user with event through user_event table
   has_many :user_events
   has_many :events, through: :user_events
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |user|
+        csv << user.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
